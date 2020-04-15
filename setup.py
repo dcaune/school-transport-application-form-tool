5318875c97533c8dcc847875f8b826ad1e3e4e20
Daniel CAUNE <daniel.caune@gmail.com>
2020-04-15 10:03:11 +0700

    Release version 1.2.4

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ __email__ = "daniel.caune@gmail.com"
 __license__ = "MIT"
 __maintainer__ = "Daniel CAUNE"
 __status__ = "Production"
-__version__ = '1.2.1'
+__version__ = '1.2.4'
 
 
 # Base directory where this file is located.
